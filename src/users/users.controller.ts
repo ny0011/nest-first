211fd87351528c917e539afe2044f6e4c53e854a
ny0011 <nayun011@gmail.com>
2022-06-08 17:59:42 +0900

    route parameter
    - :으로 해둔 건 parameter로 인식함

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -45,4 +45,12 @@ export class UsersController {
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
   }
+
+  @Delete(':userId/memo/:memoId')
+  deleteUserMemo(
+    @Param('userId') userId: string,
+    @Param('memoId') memoId: string,
+  ) {
+    return `userId: ${userId}, memoId: ${memoId}`;
+  }
 }
